809db85f5b50913ca9b9d7cf1b9c27c152a5ad04
sachinlodhi <sachinlodhi8614@gmail.com>
2022-08-22 00:08:02 +0530

    generating single code and verifying it

--- link2qr.py
+++ link2qr.py
@@ -1,10 +1,26 @@
 import csv
+import pyqrcode
+import png
 
+# distribution of the data is even i.e. 50% are benign and 50% are malicious
+benign = 'images/benign/'
+malicious = 'images/malicious/'
+ctr = 0
 # opening the CSV file
 with open('balanced_urls.csv', mode='r') as file:
     # reading the CSV file
     csvFile = csv.reader(file)
 
+
     # displaying the contents of the CSV file
     for lines in csvFile:
-        print(lines)
+        url = lines[0]
+        qr = pyqrcode.create(url)
+        if lines[2] == '0':
+            qr.png(benign+ str(ctr) + ".png", scale=8)
+        else:
+
+            qr.png(malicious + str(ctr) + ".png", scale=8)
+
+        break
+
