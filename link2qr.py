f51dae4687e540fa7f6c408d69f5b078faf25857
sachinlodhi <sachinlodhi8614@gmail.com>
2022-08-23 13:29:28 +0530

    list approach instead for csv dedicated

--- link2qr.py
+++ link2qr.py
@@ -13,26 +13,40 @@ ctrM, ctrB = -1, -1
 file = open('balanced_urls.csv', mode='r')
 
 # reading the CSV file
-csvFile = csv.reader(file)
-
+# csvFile = csv.reader(file)
 # leaving the header
-next(csvFile, None)
+# next(csvFile, None)
 
-for lines in tqdm(iterable=csvFile, desc='Progress', total=632508):
-    if ctrB ==-1 and ctrM == -1:
-        ctrM, ctrB = 0, 0
-        continue
-    url = lines[0]
-    qr = pyqrcode.create(url)
-    if lines[2] == '0':
-        qr.png(benign+ str(ctrB) + ".png", scale=8)
-        ctrB+=1
-    else:
-        qr.png(malicious + str(ctrM) + ".png", scale=8)
-        ctrM+=1
+# rading the csv rows in the line and then skipping the header
+lines = file.readlines()[1:]
+# stacking the lines in the list variable
+lines = [line.rstrip() for line in lines]
 
+# devision of the datast in 4 parts
+
+part1 = lines[1:len(lines)//4]
+part2 = lines[len(lines)//4:len(lines)//2]
+part3 = lines[len(lines)//2:len(lines)//4 * 3 ]
+part4 = lines[len(lines)//4 * 3:len(lines)]
 
 
+
+
+# for lines in tqdm(iterable=csvFile, desc='Progress', total=632508):
+#     if ctrB ==-1 and ctrM == -1:
+#         ctrM, ctrB = 0, 0
+#         continue
+#     url = lines[0]
+#     qr = pyqrcode.create(url)
+#     if lines[2] == '0':
+#         qr.png(benign+ str(ctrB) + ".png", scale=8)
+#         ctrB+=1
+#     else:
+#         qr.png(malicious + str(ctrM) + ".png", scale=8)
+#         ctrM+=1
+#
+#
+#
 
 
 
